97d89da7efb44d941175fb4a7c9db33866efaa78
sgra64 <sgraupner@beuth-hochschule.de>
2017-05-26 17:03:09 +0200

    prepared to change html

--- Vagrant_provisioning.sh
+++ Vagrant_provisioning.sh
@@ -1,32 +1,33 @@
 #!/usr/bin/env bash
-sudo apt-get update
 
+echo "\n----- Installing Java 8 JRE ------\n"
+apt-get update
 apt-get install python-software-properties -y
 add-apt-repository ppa:openjdk-r/ppa -y
 apt-get update
+apt-get install openjdk-8-jdk -y
 
-echo "\n----- Installing Apache and Java 8 ------\n"
-apt-get -y install apache2 openjdk-8-jdk
 ln -s /usr/lib/jvm/java-8-openjdk-i386 /opt/java
 
-echo "\n----- Installing Tomcat_9 ------\n"
+echo "\n----- Installing Tomcat user ------\n"
 groupadd tomcat
-useradd -m -g tomcat -p $(openssl passwd -1 lala1234) tomcat
+useradd -m -g tomcat -s /bin/bash -p $(openssl passwd -1 lala1234) tomcat
 
+echo "\n----- Installing Tomcat 9 ------\n"
 cd /opt
-wget http://archive.apache.org/dist/tomcat/tomcat-9/v9.0.0.M11/bin/apache-tomcat-9.0.0.M11.tar.gz
+wget -q http://archive.apache.org/dist/tomcat/tomcat-9/v9.0.0.M11/bin/apache-tomcat-9.0.0.M11.tar.gz
 tar xvf apache-tomcat-9.0.0.M11.tar.gz
 ln -s apache-tomcat-9.0.0.M11 tomcat
 chown -R tomcat /opt/tomcat/
 chgrp -R tomcat /opt/tomcat/
 
-chmod g+rwx /opt/tomcat/conf
-chmod g+r /opt/tomcat/conf/*
-
+#remove CR in CR/LF line ends used on Windows
 tr -d '\r' < /tmp/tomcat.conf > /etc/init/tomcat.conf
 tr -d '\r' < /tmp/webapps.manager.META-INF.context.xml > /opt/tomcat/webapps/manager/META-INF/context.xml
-sed -i 's#</tomcat-users>#  <user username="admin" password="admin" roles="manager-gui,admin-gui"/>\\n</tomcat-users>#' /opt/tomcat/conf/tomcat-users.xml
+#insert line: <user username="tomcat" password="lala1234" roles="manager-gui,admin-gui"/> in /opt/tomcat/conf/tomcat-users.xml
+sed -i 's#</tomcat-users>#  <user username="tomcat" password="lala1234" roles="manager-gui,admin-gui"/>\n</tomcat-users>#' /opt/tomcat/conf/tomcat-users.xml
 
+#tomcat start/stop scripts
 tr -d '\r' < /tmp/tomcat_ctrl > /etc/init.d/tomcat_ctrl
 echo '/etc/init.d/tomcat_ctrl start' >/etc/init.d/tomcat_start
 echo '/etc/init.d/tomcat_ctrl stop' >/etc/init.d/tomcat_stop
@@ -34,6 +35,7 @@ chmod +x /etc/init.d/tomcat_ctrl
 chmod +x /etc/init.d/tomcat_start
 chmod +x /etc/init.d/tomcat_stop
 
+#enter tomcat start/stop in run level directories
 ln -s ../init.d/tomcat_stop /etc/rc0.d/K08tomcat
 ln -s ../init.d/tomcat_stop /etc/rc6.d/K08tomcat
 
@@ -43,5 +45,8 @@ ln -s ../init.d/tomcat_start /etc/rc3.d/S92tomcat
 ln -s ../init.d/tomcat_start /etc/rc4.d/S92tomcat
 ln -s ../init.d/tomcat_start /etc/rc5.d/S92tomcat
 
-#ssh-copy-id tomcat@192.168.33.20
+echo "\n----- Start Tomcat 9 ------\n"
+/etc/init.d/tomcat_start
+
+#ssh-copy-id tomcat@192.168.3.16
 #tar -C target -c target/hello-spring.war | ssh vagrant@192.168.3.16 "sudo tar -x -C /opt/tomcat/webapps"
--- src/main/webapp/pages/hello.jsp
+++ src/main/webapp/pages/hello.jsp
@@ -1,5 +1,8 @@
 <html>
-<body>
-	<h1>${message}</h1>
-</body>
+	<head>
+		<!-- link rel="stylesheet" href="css/Styles.css" -->
+	</head>
+	<body>
+		<h1>${message}</h1>
+	</body>
 </html>
